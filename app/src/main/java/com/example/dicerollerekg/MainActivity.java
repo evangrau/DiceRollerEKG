6abc67e6b3c1fed2843185ec53f1ea3fe208316e
Evan Grau <89275830+whatissatire@users.noreply.github.com>
2022-09-22 09:03:20 -0500

    starting d20

--- app/src/main/java/com/example/dicerollerekg/MainActivity.java
+++ app/src/main/java/com/example/dicerollerekg/MainActivity.java
@@ -2,20 +2,47 @@ package com.example.dicerollerekg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.view.View;
 import android.widget.ImageView;
 
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
     private ImageView imageViewDice;
     private Random rng = new Random();
+
+    // Device shake stuff
+    private SensorManager sensorManager;
+    private Sensor accelerometerSensor;
+    private boolean isAccelerometerSensorAvailable, isNotFirstRun = false;
+    private float lastX;
+    private float lastY;
+    private float lastZ;
+    private Vibrator vibrator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
+            accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            isAccelerometerSensorAvailable = true;
+        }
+        else {
+            isAccelerometerSensorAvailable = false;
+        }
 
         imageViewDice = findViewById(R.id.image_view_dice);
         imageViewDice.setOnClickListener(new View.OnClickListener() {
@@ -48,6 +75,50 @@ public class MainActivity extends AppCompatActivity {
             case 6:
                 imageViewDice.setImageResource(R.drawable.dice6);
                 break;
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        float currentX = sensorEvent.values[0];
+        float currentY = sensorEvent.values[1];
+        float currentZ = sensorEvent.values[2];
+
+        if (isNotFirstRun) {
+            float xDifference = Math.abs(lastX - currentX);
+            float yDifference = Math.abs(lastY - currentY);
+            float zDifference = Math.abs(lastZ - currentZ);
+
+            float shakeThreshold = 5f;
+            if ((xDifference > shakeThreshold && yDifference > shakeThreshold) || (xDifference > shakeThreshold && zDifference > shakeThreshold) || (yDifference > shakeThreshold && zDifference > shakeThreshold)) {
+                vibrator.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
+            }
+        }
+
+        lastX = currentX;
+        lastY = currentY;
+        lastZ = currentZ;
+        isNotFirstRun = true;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (isAccelerometerSensorAvailable) {
+            sensorManager.registerListener(this, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (isAccelerometerSensorAvailable) {
+            sensorManager.unregisterListener(this);
         }
     }
 }
