1b3124c3bff991a776dc15b7909e8b5e60094c8c
Evan Grau <89275830+whatissatire@users.noreply.github.com>
2022-09-23 13:27:25 -0500

    added new dice and new splash screen logo

--- app/src/main/java/com/example/dicerollerekg/MainActivity.java
+++ app/src/main/java/com/example/dicerollerekg/MainActivity.java
@@ -73,28 +73,70 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     }
 
     private void rollDice() {
-        int randomNumber = rng.nextInt(6) + 1;
+        int randomNumber = rng.nextInt(20) + 1;
         boolean hit, miss;
 
         switch (randomNumber) {
             case 1:
-                imageViewDice.setImageResource(R.drawable.dice1);
+                imageViewDice.setImageResource(R.drawable.d20_1);
                 showMiss();
                 break;
             case 2:
-                imageViewDice.setImageResource(R.drawable.dice2);
+                imageViewDice.setImageResource(R.drawable.d20_2);
                 break;
             case 3:
-                imageViewDice.setImageResource(R.drawable.dice3);
+                imageViewDice.setImageResource(R.drawable.d20_3);
                 break;
             case 4:
-                imageViewDice.setImageResource(R.drawable.dice4);
+                imageViewDice.setImageResource(R.drawable.d20_4);
                 break;
             case 5:
-                imageViewDice.setImageResource(R.drawable.dice5);
+                imageViewDice.setImageResource(R.drawable.d20_5);
                 break;
             case 6:
-                imageViewDice.setImageResource(R.drawable.dice6);
+                imageViewDice.setImageResource(R.drawable.d20_6);
+                break;
+            case 7:
+                imageViewDice.setImageResource(R.drawable.d20_7);
+                break;
+            case 8:
+                imageViewDice.setImageResource(R.drawable.d20_8);
+                break;
+            case 9:
+                imageViewDice.setImageResource(R.drawable.d20_9);
+                break;
+            case 10:
+                imageViewDice.setImageResource(R.drawable.d20_10);
+                break;
+            case 11:
+                imageViewDice.setImageResource(R.drawable.d20_11);
+                break;
+            case 12:
+                imageViewDice.setImageResource(R.drawable.d20_12);
+                break;
+            case 13:
+                imageViewDice.setImageResource(R.drawable.d20_13);
+                break;
+            case 14:
+                imageViewDice.setImageResource(R.drawable.d20_14);
+                break;
+            case 15:
+                imageViewDice.setImageResource(R.drawable.d20_15);
+                break;
+            case 16:
+                imageViewDice.setImageResource(R.drawable.d20_16);
+                break;
+            case 17:
+                imageViewDice.setImageResource(R.drawable.d20_17);
+                break;
+            case 18:
+                imageViewDice.setImageResource(R.drawable.d20_18);
+                break;
+            case 19:
+                imageViewDice.setImageResource(R.drawable.d20_19);
+                break;
+            case 20:
+                imageViewDice.setImageResource(R.drawable.d20_20);
                 showHit();
                 break;
         }
